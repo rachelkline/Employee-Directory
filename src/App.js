46e61fc62844cc330687f53c78e72a202d80a70d
Rachel Kline <rach3lkline@gmail.com>
2020-10-29 19:52:45 -0400

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
+import React from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <>
+    <h1>TEST</h1>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button
+            className="navbar-toggler"
+            type="button">
+            </button>
+            <div className="search-area">
+                {/* <SearchName /> */}
+            </div>
+        </nav>
+    )
+}
