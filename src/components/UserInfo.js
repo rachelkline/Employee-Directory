aaadeeb06ce9309e890a51036eaa1d02aba9198a
Rachel Kline <rach3lkline@gmail.com>
2020-11-02 13:14:11 -0500

    Fixed Sort Functionality

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -12,10 +12,10 @@ const UserInfo = () => {
     order: "descend",
     filteredUsers: [],
     headings: [
-      { name: "Icon Image", width: "10%", order: "descend" },
+      { name: "image", width: "10%", order: "descend" },
       { name: "name", width: "10%", order: "descend" },
-      { name: "Phone Number", width: "20%", order: "descend" },
-      { name: "Email Address", width: "20%", order: "descend" },
+      { name: "phone number", width: "20%", order: "descend" },
+      { name: "email", width: "20%", order: "descend" },
       { name: "dob", width: "10%", order: "descend" }
     ]
   });
